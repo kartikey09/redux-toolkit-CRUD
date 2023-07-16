ebb3e48c2fd69e9bd595ff1a1ffbb12202282489
karikey rana <rana.kartikey999@gmail.com>
2023-07-16 07:29:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <StrictMode>
+
+    </StrictMode>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
