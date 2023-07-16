82b76c76832072a65b2e2459877c89374884030d
karikey rana <rana.kartikey999@gmail.com>
2023-07-16 18:19:14 +0530

    16 - redux thunk middleware

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,9 @@
 const redux = require("redux");
 const produce = require("immer").produce;
+const axios = require("axios");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
 
 const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
 const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
@@ -13,47 +17,65 @@ const initialState = {
 
 const fetchUsersRequested = () => {
   return {
-    action: FETCH_USERS_REQUESTED,
+    type: FETCH_USERS_REQUESTED,
   };
 };
 
 const fetchUsersSuccess = (users) => {
   return {
-    action: FETCH_USERS_SUCCESS,
+    type: FETCH_USERS_SUCCESS,
     payload: users,
   };
 };
 
 const fetchUsersFailure = (error) => {
   return {
-    action: FETCH_USERS_FAILURE,
+    type: FETCH_USERS_FAILURE,
     payload: error,
   };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_USERS_REQUESTED: {
+    case FETCH_USERS_REQUESTED:
       return produce(state, (draft) => {
         draft.loading = true;
       });
-    }
 
-    case FETCH_USERS_SUCCESS: {
+    case FETCH_USERS_SUCCESS:
       return produce(state, (draft) => {
         draft.loading = false;
-        draft.users = users;
+        draft.users = action.payload;
       });
-    }
 
-    case FETCH_USERS_FAILURE: {
+    case FETCH_USERS_FAILURE:
       return produce(state, (draft) => {
         draft.loading = false;
         draft.error = action.payload;
         draft.users = [];
       });
-    }
+
     default:
       return state;
   }
 };
+
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequested());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const usr = res.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(usr));
+      })
+      .catch((err) => {
+        dispatch(fetchUsersFailure(err.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(fetchUsers());
