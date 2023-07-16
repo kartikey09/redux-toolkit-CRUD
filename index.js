59e9304bd30a9d8c811d9df90435de7123d53e73
karikey rana <rana.kartikey999@gmail.com>
2023-07-16 10:40:23 +0530

    11 - Combining Multiple Reducers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const {configureStore} = require('@reduxjs/toolkit');
+
 const CAKE_ORDRED = 'CAKE_ORDERED';
-const CAKE_RESTOCK = 'CAKE_RESTOCK'
+const CAKE_RESTOCK = 'CAKE_RESTOCK';
+const ICECREAM_ORDER = 'ICECREAM_ORDER';
+const ICECREAM_RESTOCK = 'ICECREAM_RESTOCK';
 
 function orderCake(){
     return {
@@ -16,11 +19,29 @@ function restockCake(qty = 1){
     }
 }
 
-const initialState = { 
+const initialCakeState = { 
      numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) =>{
+function orderIcecream(){
+    return {
+        type: ICECREAM_ORDER,
+        quantity: 1
+    }
+}
+
+function restockIcecream(qty = 1){
+    return{
+        type: ICECREAM_RESTOCK,
+        payload: qty
+    }
+}
+
+const initialIcecreamState = { 
+    numOfIcecreams: 20
+}
+
+const Cakereducer = (state = initialCakeState, action) =>{
     switch(action.type){
         case CAKE_ORDRED: return {
             ...state,
@@ -36,13 +57,38 @@ const reducer = (state = initialState, action) =>{
     }
 }
 
-const store = configureStore({reducer});
+const Icecreamreducer = (state = initialIcecreamState, action) =>{
+    switch(action.type){
+        case ICECREAM_ORDER: return {
+            ...state,
+            numOfIcecreams : state.numOfIcecreams - 1
+        }
+
+        case ICECREAM_RESTOCK: return{
+            ...state,
+            numOfIcecreams: action.payload
+        }
+        default: 
+            return state
+    }
+}
+
+const store = configureStore({reducer: {
+    cake: Cakereducer,
+    icecream: Icecreamreducer
+}});
+
 console.log('Initial state : ', store.getState());
 const unsubscribe = store.subscribe(()=> console.log('Updated State: ', store.getState()));
+
 
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(restockCake(10));
+store.dispatch(orderIcecream());
+store.dispatch(orderIcecream());
+store.dispatch(orderIcecream());
+store.dispatch(restockIcecream(20));
 
 unsubscribe();
