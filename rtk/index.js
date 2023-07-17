572494dcfd291e5bbf0bc982bda3435ba0eba852
karikey rana <rana.kartikey999@gmail.com>
2023-07-17 07:59:43 +0530

    24 - Async Thunks

--- rtk/index.js
+++ rtk/index.js
@@ -1,16 +1,18 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions;
+const {fetchUsers} = require('./features/user/userSlice')
 
 const unsubscribe = store.subscribe(() => {});
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(3));
+store.dispatch(fetchUsers());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(3));
 
-unsubscribe();
+// unsubscribe();
